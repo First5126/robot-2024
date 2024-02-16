578b87f224bf2bd757c60bb75fcb6ff3e40c109d
Alex2007M <156712424+Alex2007M@users.noreply.github.com>
2024-02-16 09:59:46 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/commands/ArmIn.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.robotArm;
+
+public class ArmIn extends Command {
+
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final robotArm m_subsystem;
+  private boolean isFinished = false;
+  private boolean Slowed;
+
+  public ArmIn(robotArm subsystem) {
+    m_subsystem = subsystem;
+  }
+
+  @Override
+  public void initialize() {
+    isFinished = false;
+    Slowed = false;
+  }
+
+  @Override
+  public void execute() {
+    if (!m_subsystem.SlowSensor.get()){
+      Slowed = true;
+    }
+    if (!m_subsystem.StopSensor.get()){
+      isFinished = true;
+    }
+    else if (Slowed){
+      m_subsystem.enableIntakeslow();
+    }
+    else{
+      m_subsystem.enableIntake();
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.disableIntake();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return isFinished;
+  }
+}
---
+++ src/main/java/frc/robot/commands/ClearJam.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.robotArm;
+
+public class ClearJam extends Command {
+
+  private final robotArm m_subsystem;
+  private boolean isFinished;
+
+  public ClearJam(robotArm subsystem) {
+    m_subsystem = subsystem;
+  }
+
+  @Override
+  public void initialize() {
+    isFinished = false;
+  }
+
+  @Override
+  public void execute() {
+    m_subsystem.enableClearJam();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.disableClearJam();
+    isFinished = true;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return isFinished;
+  }
+}
