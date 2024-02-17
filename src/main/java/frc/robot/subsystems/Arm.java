94d0f54802f5ea7ff367a0713df137740dae1606
RoWiz09 <139277027+RoWiz09@users.noreply.github.com>
2024-02-17 09:32:04 -0600

    removed unused imports and variables from the code

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.wpilibj.DigitalSource;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,9 +16,8 @@ public class Arm extends SubsystemBase {
   private final PositionVoltage positionVoltage;
   private final TalonFX leftMotorFx;
   private final TalonFX rightMotorFx;
+
   private final Encoder encoder;
-  private double motorVal;
-  private boolean spinning = false;
   public Arm() {
     slot0Configs = new Slot0Configs();
       slot0Configs.kP = Constants.armConstants.kP;
@@ -29,12 +27,11 @@ public class Arm extends SubsystemBase {
       slot0Configs.kG = Constants.armConstants.kG;
       slot0Configs.kS = Constants.armConstants.kS;
       slot0Configs.kV = Constants.armConstants.kV;
+
     positionVoltage = new PositionVoltage(0).withSlot(0);
     leftMotorFx = new TalonFX(11, "frc5126");
-    rightMotorFx = new TalonFX(10, "frc5126"); // changing to 12
-    // rightMotorFx.setInverted(true);
     leftMotorFx.getConfigurator().apply(slot0Configs);
-    // rightMotorFx.getConfigurator().apply(slot0Configs);
+    rightMotorFx = new TalonFX(10, "frc5126"); // changing to 12
 
     encoder = new Encoder(0, 1);
   }
