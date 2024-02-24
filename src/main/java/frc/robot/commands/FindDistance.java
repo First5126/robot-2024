4c41b1fbd5c9e8134fbca31383f390750d491e12
RoWiz09 <139277027+RoWiz09@users.noreply.github.com>
2024-02-24 14:08:52 -0600

    Made limelight things

--- src/main/java/frc/robot/commands/FindDistance.java
+++ src/main/java/frc/robot/commands/FindDistance.java
@@ -1,15 +1,17 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.LLIntake;
+import frc.robot.subsystems.LLSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
 public class FindDistance extends Command {
-  private final LLIntake m_LLSubsystem;
-  private final boolean IntakeLL;
+  private final LLSubsystem m_LLSubsystem;
+  private final int IntakeLL;
+  private double distance;
 
   private boolean isFinished;
-  public FindDistance(LLIntake LL_subsystem, boolean Front_LL) {
+  public FindDistance(LLSubsystem LL_subsystem, int Front_LL) {
     m_LLSubsystem = LL_subsystem;
     IntakeLL = Front_LL;
   }
@@ -23,16 +25,19 @@ public class FindDistance extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    distance = m_LLSubsystem.getDistance(IntakeLL);
+    isFinished = true;
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    isFinished = true;
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return isFinished;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/LLSubsystem.java
@@ -0,0 +1,179 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class LLSubsystem extends SubsystemBase {
+  NetworkTable IntakeLL = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry IntakeTX;
+  NetworkTableEntry IntakeTY;
+  NetworkTableEntry IntakeTA; 
+  NetworkTableEntry IntakeTID;
+
+  NetworkTable BackLL = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry BackTX;
+  NetworkTableEntry BackTY;
+  NetworkTableEntry BackTA; 
+  NetworkTableEntry BackTID;
+
+  NetworkTable FrontLL = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry FrontTX;
+  NetworkTableEntry FrontTY;
+  NetworkTableEntry FrontTA; 
+  NetworkTableEntry FrontTID;
+
+  double intakeLimelightAngle;
+
+  double IntakeX;
+  double IntakeY;
+  double IntakeArea;
+  long IntakeId;
+
+  double backLimelightAngle;
+
+  double BackX;
+  double BackY;
+  double BackArea;
+  long BackId;
+
+  double FrontLimelightAngle;
+
+  double FrontX;
+  double FrontY;
+  double FrontArea;
+  long FrontId;
+
+  // distance from the center of the Limelight lens to the floor in inches
+  double intakeLimelightHeight = 31.0; 
+  double backLimelightHeight = 31.0;
+  double FrontLimelightHeight = 31.0;
+
+  double IntakeDistanceFromTarget = 0;
+  double BackDistanceFromTarget = 0;
+  double FrontDistanceFromTarget = 0;
+
+  public LLSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // Gets Intake LimeLight reading data
+    IntakeTX = IntakeLL.getEntry("tx");
+    IntakeTY = IntakeLL.getEntry("ty");
+    IntakeTA = IntakeLL.getEntry("ta");
+    IntakeTID = IntakeLL.getEntry("tid");
+    
+    // Gets Back LimeLight reading data
+    BackTX = BackLL.getEntry("tx");
+    BackTY = BackLL.getEntry("ty");
+    BackTA = BackLL.getEntry("ta");
+    BackTID = BackLL.getEntry("tid");
+
+    // Gets Front LimeLight reading data
+    FrontTX = BackLL.getEntry("tx");
+    FrontTY = BackLL.getEntry("ty");
+    FrontTA = BackLL.getEntry("ta");
+    FrontTID = BackLL.getEntry("tid");
+
+    // reads Inttake Limelight values periodically
+    IntakeX = IntakeTX.getDouble(0.0);
+    IntakeY = IntakeTY.getDouble(0.0);
+    IntakeArea = IntakeTA.getDouble(0.0);
+    IntakeId = IntakeTID.getInteger(0);   
+
+    // reads Back Limelight values periodically
+    BackX = BackTX.getDouble(0.0);
+    BackY = BackTY.getDouble(0.0);
+    BackArea = BackTA.getDouble(0.0);
+    BackId = BackTID.getInteger(0);  
+    
+    // reads Front Limelight values periodically
+    FrontX = BackTX.getDouble(0.0);
+    FrontY = BackTY.getDouble(0.0);
+    FrontArea = BackTA.getDouble(0.0);
+    FrontId = BackTID.getInteger(0);
+
+    // Intake Limelight Values
+    SmartDashboard.putNumber("(Inttake) Limelight IntakeTX", IntakeX);
+    SmartDashboard.putNumber("(Inttake) Limelight IntakeTY", IntakeY);
+    SmartDashboard.putNumber("(Inttake) Limelight Area", IntakeArea);
+    SmartDashboard.putNumber("(Inttake) Target Distance", IntakeDistanceFromTarget);
+    intakeLimelightAngle = SmartDashboard.getNumber("(Intake) Limelight Angle", 9);
+    
+    // Back Limelight Values
+    SmartDashboard.putNumber("(Back) Limelight tx", BackX);
+    SmartDashboard.putNumber("(Back) Limelight ty", BackY);
+    SmartDashboard.putNumber("(Back) Limelight Area", BackArea);
+    SmartDashboard.putNumber("(Back) Target Distance", BackDistanceFromTarget);
+    backLimelightAngle = SmartDashboard.getNumber("(Back) Limelight Angle", 9);
+
+    // Front Limelight Values
+    SmartDashboard.putNumber("(Front) Limelight tx", FrontX);
+    SmartDashboard.putNumber("(Front) Limelight ty", FrontY);
+    SmartDashboard.putNumber("(Front) Limelight Area", FrontArea);
+    SmartDashboard.putNumber("(Front) Target Distance", FrontDistanceFromTarget);
+    FrontLimelightAngle = SmartDashboard.getNumber("(Front) Limelight Angle", 9);
+  }
+
+  public double getDistance(int LLId) { 
+    double targetDegrees;
+    double targetHeight = 0;
+    if (LLId == 1){
+      if (IntakeId == 1 || IntakeId == 2 || IntakeId == 5 || IntakeId == 6 || IntakeId == 9 || IntakeId == 10) {
+        targetHeight = 9.375;
+      }
+      else if (IntakeId == 3 || IntakeId == 4 || IntakeId == 7 || IntakeId == 8) {
+        targetHeight = 57.125;
+      }
+      else if (IntakeId >= 11) {
+        targetHeight = 52;
+      }
+      targetDegrees = intakeLimelightAngle + IntakeY;
+      double targetRadians = targetDegrees * (3.14159 / 180.0);
+
+      // calculates distance
+      IntakeDistanceFromTarget = (targetHeight - intakeLimelightHeight) / Math.tan(targetRadians);
+      return IntakeDistanceFromTarget;
+    }
+    else if (LLId == 2){
+      if (BackId == 1 || BackId == 2 || BackId == 5 || BackId == 6 || BackId == 9 || BackId == 10) {
+        targetHeight = 9.375;
+      }
+      else if (BackId == 3 || BackId == 4 || BackId == 7 || BackId == 8) {
+        targetHeight = 57.125;
+      }
+      else if (BackId >= 11) {
+        targetHeight = 52;
+      }
+      targetDegrees = backLimelightAngle + BackY;
+      double targetRadians = targetDegrees * (3.14159 / 180.0);
+
+      // calculates distance
+      BackDistanceFromTarget = (targetHeight - backLimelightHeight) / Math.tan(targetRadians);
+      return BackDistanceFromTarget;
+    }
+    else if (LLId == 3){
+      if (FrontId == 1 || FrontId == 2 || FrontId == 5 || FrontId == 6 || FrontId == 9 || FrontId == 10) {
+        targetHeight = 9.375;
+      }
+      else if (FrontId == 3 || FrontId == 4 || FrontId == 7 || FrontId == 8) {
+        targetHeight = 57.125;
+      }
+      else if (FrontId >= 11) {
+        targetHeight = 52;
+      }
+      targetDegrees = FrontLimelightAngle + BackY;
+      double targetRadians = targetDegrees * (3.14159 / 180.0);
+
+      // calculates distance
+      FrontDistanceFromTarget = (targetHeight - FrontLimelightHeight) / Math.tan(targetRadians);
+      return FrontDistanceFromTarget;
+    }
+    else{
+      return -1;
+    }
+  }
+}
